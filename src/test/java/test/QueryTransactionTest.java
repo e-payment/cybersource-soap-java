405ecd2925637256721d78ebd05676264534b99f
Junlapong L <junlapong@gmail.com>
2018-08-18 23:04:38 +0700

    update query transaction

--- src/test/java/test/QueryTransactionTest.java
+++ src/test/java/test/QueryTransactionTest.java
@@ -1,10 +1,26 @@
 package test;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,10 +54,41 @@ public class QueryTransactionTest extends CyberSourceBaseTest {
 
 		HttpUtils.debugSSL(false, "ssl:handsahake");
 	}
+	
+	@Test
+	public void shouldQueryByRequestId1() throws ClientProtocolException, IOException, AuthenticationException {
+		
+		setup();
+		
+		String requestID = "5295543636306097804101";
+		
+	    HttpPost httpPost = new HttpPost(REPORT_URL);
+	    
+	    UsernamePasswordCredentials creds = new UsernamePasswordCredentials("itcybs", "Password101");
+	    httpPost.addHeader(new BasicScheme().authenticate(creds, httpPost, null));
+	    
+	    List<NameValuePair> postData = new ArrayList<>();
+		postData.add(new BasicNameValuePair("merchantID", MERCHANT_ID));
+		postData.add(new BasicNameValuePair("type", "transaction"));
+		postData.add(new BasicNameValuePair("subtype", "transactionDetail"));
+		postData.add(new BasicNameValuePair("requestID", requestID));
+		httpPost.setEntity(new UrlEncodedFormEntity(postData));
+
+		//HttpResponse response = HttpClients.createDefault().execute(httpPost);
+
+	    CloseableHttpClient client = HttpClients.createDefault();
+	    CloseableHttpResponse response = client.execute(httpPost);
+	    
+	    //int code = response.getStatusLine().getStatusCode();
+	    log.debug("code: {}", response.getStatusLine());
+	    log.debug("\n{}", EntityUtils.toString(response.getEntity()));
+	    
+	    client.close();
+	}
 
 	@Test
 	@Ignore
-	public void shouldQueryByRequestId() throws Exception {
+	public void shouldQueryByRequestId2() throws Exception {
 
 		setup();
 		
@@ -50,15 +97,9 @@ public class QueryTransactionTest extends CyberSourceBaseTest {
 
 		// <requestID> is a 22 digit numeric ID corresponding to a transaction in the
 		// CyberSource system
-		String requestID = "5106476186716067204105";
+		String requestID = "5295543636306097804101";
+		getTxnRecord(requestID);
 
-		try {
-
-			getTxnRecord(requestID);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			log.error("ERROR: {}", ex.getMessage());
-		}
 	}
 
 	/*
@@ -70,7 +111,7 @@ public class QueryTransactionTest extends CyberSourceBaseTest {
 	private static void getTxnRecord(String requestID) throws Exception {
 
 		String rp_username = "itcybs";
-		String rp_password = "Password100";
+		String rp_password = "Password101";
 
 		// Construct post data
 		StringBuffer postData = new StringBuffer();
@@ -88,20 +129,6 @@ public class QueryTransactionTest extends CyberSourceBaseTest {
 		// Encode login info
 		String userCredentials = rp_username + ":" + rp_password;
 		String basicAuth = "Basic " + Base64.getEncoder().encodeToString(userCredentials.getBytes());
-
-		// curl command
-		if (log.isDebugEnabled()) {
-			StringBuilder curlCmd = new StringBuilder();
-			curlCmd.append("curl -kv -POST");
-			curlCmd.append(" -x ").append(config.getString("proxy.host")).append(":").append(config.getInt("proxy.port"));
-			curlCmd.append(" --url ").append(REPORT_URL);
-			curlCmd.append(" --header \"Authorization: ").append(basicAuth).append("\"");
-			curlCmd.append(" -d \"merchantID=").append(MERCHANT_ID);
-			curlCmd.append(" -d \"type=transaction");
-			curlCmd.append(" -d \"subtype=transactionDetail");
-			curlCmd.append(" -d \"requestID=").append(requestID);
-			log.debug("\n{}\n", curlCmd);
-		}
 		
 		log.debug("> {}", basicAuth);
 
